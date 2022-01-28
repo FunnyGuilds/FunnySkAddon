510bc60940e65409cc47e351ffa2fa9bf4cf3fba
Peridot <peridot491@protonmail.com>
2022-01-28 01:20:24 +0100

    Release 2.2.15-Beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "pl.funnyskaddon"
-version = "2.2.14-Beta"
+version = "2.2.15-Beta"
 
 tasks.withType<ShadowJar> {
     archiveFileName.set("FunnySkAddon-${project.version}.jar")
