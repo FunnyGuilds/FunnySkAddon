26a3e71821aadd454c073a7582d43a6882610d35
Peridot <peridot491@protonmail.com>
2022-02-05 23:27:21 +0100

    Improve SkriptString.kt

--- src/main/kotlin/pl/funnyskaddon/skript/SkriptString.kt
+++ src/main/kotlin/pl/funnyskaddon/skript/SkriptString.kt
@@ -1,7 +1,17 @@
 package pl.funnyskaddon.skript
 
 class SkriptString(val string: String) {
+
+    override fun hashCode(): Int {
+        return string.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return string == other
+    }
+
     override fun toString(): String {
         return string
     }
+
 }
