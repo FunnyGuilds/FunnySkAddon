ceb38a72e196284ddad1f1487bebca198c9ddd7f
Peridot <peridot491@protonmail.com>
2023-02-20 05:32:25 +0100

    Update okaeri-configs to 5.0.0-beta.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
     implementation("org.bstats:bstats-bukkit:3.0.1")
     implementation("commons-io:commons-io:2.11.0")
-    implementation("eu.okaeri:okaeri-configs-yaml-bukkit:4.0.9")
+    implementation("eu.okaeri:okaeri-configs-yaml-bukkit:5.0.0-beta.3")
     shadow("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
     shadow("net.dzikoysk.funnyguilds:plugin:4.11.0")
     shadow("com.github.SkriptLang:Skript:2.6.2") {
